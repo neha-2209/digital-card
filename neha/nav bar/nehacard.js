be0034b9031e700d0d78119c017333a8930fd458
neha-2209 <96404597+neha-2209@users.noreply.github.com>
2021-12-22 05:31:27 -0800

    tab bar

--- neha/nav bar/nehacard.js
+++ neha/nav bar/nehacard.js
@@ -13,6 +13,8 @@ links.forEach((link, index) =>
     // {
 	// 	light.style.left = `${links[index].offsetLeft + light.offsetWidth / 4}px`;
 	// }
+
+
 	link.addEventListener("click", (e) => 
     {
 		activeIndex = index;
@@ -53,3 +55,22 @@ function openMenu(evt, menuName){
 	document.getElementById(menuName).style.display = "block";
 	evt.currentTarget.className += "active";
 }
+
+
+
+
+// let tabHeader = document.getElementsByClassName("tab-header")[0];
+// let tabIndicator = document.getElementsByClassName("tab-indicator")[0];
+// let tabBody = document.getElementsByClassName("tab-body")[0];
+
+// lettabsPane = tabHeader.getElementsByTagName("div");
+
+// for(let i=0; i<tabsPane.length; i++){
+// 	tabsPane[i].addEventListener("click", function(){
+// 		tabHeader.getElementsByClassName("active")[0].classList.remove("active");
+// 		tabsPane[i].classList.add("active")	
+
+// 		tabBody.getElementsByClassName("active")[0].classList.remove("active");
+// 		tabBody.getElementsByTagName("div")[i].classList.add("active");
+// 	});
+// }
