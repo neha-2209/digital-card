1710511eb3e3a8d3d8e7470b7e108886cae59020
neha-2209 <96404597+neha-2209@users.noreply.github.com>
2021-12-23 05:39:56 -0800

    bank page

--- neha/nav bar/nehacard.js
+++ neha/nav bar/nehacard.js
@@ -59,14 +59,14 @@ links.forEach((link, index) =>
 
 
 
-const tabs = document.querySelectorAll('[data-tab-target')
+// const tabs = document.querySelectorAll('[data-tab-target')
 
-tabs.forEach (tab => {
-	tab.addEventListener('click',() => {
-		const target = document.querySelector(tab.dataset.tabTarget)
-		target.classList.add('active')
-	})
-})
+// tabs.forEach (tab => {
+// 	tab.addEventListener('click',() => {
+// 		const target = document.querySelector(tab.dataset.tabTarget)
+// 		target.classList.add('active')
+// 	})
+// })
 
 
 
@@ -86,3 +86,31 @@ tabs.forEach (tab => {
 // 		tabBody.getElementsByTagName("div")[i].classList.add("active");
 // 	});
 // }
+
+
+
+
+
+
+function openCity(evt, cityName) {
+	// Declare all variables
+	var i, tabcontent, tablinks;
+  
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+  
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+  
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
+
+	document.getElementById("defaultOpen").click();
+  }
