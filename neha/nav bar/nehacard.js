3ad0be3dca3a3e0e247e0facee3f8869e6e76f15
neha-2209 <96404597+neha-2209@users.noreply.github.com>
2021-12-31 05:37:34 -0800

    catelog change

--- neha/nav bar/nehacard.js
+++ neha/nav bar/nehacard.js
@@ -119,33 +119,102 @@ function openCity(evt, cityName) {
 
 
 
-
-
-
-
-
-
-
-  var slideIndex = 0;
-  showSlides();
+  var gallery = {
   
-  function showSlides() {
-	var i;
-	var slides = document.getElementsByClassName("mySlides");
-	var dots = document.getElementsByClassName("dot");
-	for (i = 0; i < slides.length; i++) {
-	  slides[i].style.display = "none";  
+	currentImage: 0,
+	images: document.querySelectorAll('.gallery--image'),
+	scrollContainer: document.querySelector('.gallery--images'),
+	buttonPrev: document.querySelector('.gallery--button.-previous'),
+	buttonNext: document.querySelector('.gallery--button.-next'),
+	
+	updateDisabledButtons: function() {
+	  this.buttonPrev.disabled = !!(this.currentImage === 0);
+	  this.buttonNext.disabled = !!(this.currentImage === this.images.length -1);
+	},
+	
+	scrollToCurrentImage: function() {
+	  let currentImage = this.images[this.currentImage];
+	  let container = this.scrollContainer;
+	  let imagePositionCenter = Math.floor(currentImage.offsetLeft - ((container.offsetWidth  - currentImage.offsetWidth) / 2));
+	  
+	  // we need to toggle the CSS scroll-snap
+	  // otherwise the gallery occassionally doesn't slide to the correct new position
+	  // this doesn't seem to be an issue if we scroll-snap to the left
+	  container.classList.remove('-scroll-snap');
+	  container.scrollLeft = imagePositionCenter;
+	  setTimeout(function() { container.classList.add('-scroll-snap') }, 500);
+	},
+	
+	navigate: function(_direction) {
+	  if ((_direction === -1 && this.currentImage > 0) ||
+		  (_direction === 1 && this.currentImage < (this.images.length - 1))) {
+		this.images[this.currentImage].classList.remove('-current');
+		this.currentImage += _direction;
+		this.images[this.currentImage].classList.add('-current');
+		this.scrollToCurrentImage();
+		this.updateDisabledButtons();
+	  }
+	},
+  
+	init: function() {
+	  this.scrollContainer.scrollLeft = 0;
+	  
+	  document.querySelector('.gallery--button.-previous').addEventListener('click', function(){
+		gallery.navigate(-1);
+	  });
+  
+	  document.querySelector('.gallery--button.-next').addEventListener('click', function(){
+		gallery.navigate(1);
+	  });
 	}
-	slideIndex++;
-	if (slideIndex > slides.length) {slideIndex = 1}    
-	for (i = 0; i < dots.length; i++) {
-	  dots[i].className = dots[i].className.replace(" active", "");
-	}
-	slides[slideIndex-1].style.display = "block";  
-	dots[slideIndex-1].className += " active";
-	setTimeout(showSlides, 2000); // Change image every 2 seconds
-  }
+  };
+  
+  // just waiting for the placeholder images to load ...
+  setTimeout(function() { gallery.init(); }, 1000);
+  
 
 
+
+
+
+
+//   var slideIndex = 0;
+//   showSlides();
+  
+//   function showSlides() {
+// 	var i;
+// 	var slides = document.getElementsByClassName("mySlides");
+// 	var dots = document.getElementsByClassName("dot");
+// 	for (i = 0; i < slides.length; i++) {
+// 	  slides[i].style.display = "none";  
+// 	}
+// 	slideIndex++;
+// 	if (slideIndex > slides.length) {slideIndex = 1}    
+// 	for (i = 0; i < dots.length; i++) {
+// 	  dots[i].className = dots[i].className.replace(" active", "");
+// 	}
+// 	slides[slideIndex-1].style.display = "block";  
+// 	dots[slideIndex-1].className += " active";
+// 	setTimeout(showSlides, 2000); // Change image every 2 seconds
+//   }
+
+
+  $(function() {
+  
+	// contact form animations
+	$('#contact').click(function() {
+	  $('#contactForm').fadeToggle();
+	})
+	$(document).mouseup(function (e) {
+	  var container = $("#contactForm");
+  
+	  if (!container.is(e.target) // if the target of the click isn't the container...
+		  && container.has(e.target).length === 0) // ... nor a descendant of the container
+	  {
+		  container.fadeOut();
+	  }
+	});
+	
+  });
 
 
