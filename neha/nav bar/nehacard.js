021ef37373894a2ec836f0b38aee6bae66c91db5
neha-2209 <96404597+neha-2209@users.noreply.github.com>
2022-01-03 05:43:15 -0800

    final card

--- neha/nav bar/nehacard.js
+++ neha/nav bar/nehacard.js
@@ -93,28 +93,21 @@ links.forEach((link, index) =>
 
 
 function openCity(evt, cityName) {
-	// Declare all variables
 	var i, tabcontent, tablinks;
-  
-	// Get all elements with class="tabcontent" and hide them
 	tabcontent = document.getElementsByClassName("tabcontent");
 	for (i = 0; i < tabcontent.length; i++) {
 	  tabcontent[i].style.display = "none";
 	}
-  
-	// Get all elements with class="tablinks" and remove the class "active"
 	tablinks = document.getElementsByClassName("tablinks");
 	for (i = 0; i < tablinks.length; i++) {
 	  tablinks[i].className = tablinks[i].className.replace(" active", "");
 	}
-  
-	// Show the current tab, and add an "active" class to the button that opened the tab
 	document.getElementById(cityName).style.display = "block";
 	evt.currentTarget.className += " active";
-
-	document.getElementById("defaultOpen").click();
   }
-
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
 
 
 
@@ -178,27 +171,6 @@ function openCity(evt, cityName) {
 
 
 
-//   var slideIndex = 0;
-//   showSlides();
-  
-//   function showSlides() {
-// 	var i;
-// 	var slides = document.getElementsByClassName("mySlides");
-// 	var dots = document.getElementsByClassName("dot");
-// 	for (i = 0; i < slides.length; i++) {
-// 	  slides[i].style.display = "none";  
-// 	}
-// 	slideIndex++;
-// 	if (slideIndex > slides.length) {slideIndex = 1}    
-// 	for (i = 0; i < dots.length; i++) {
-// 	  dots[i].className = dots[i].className.replace(" active", "");
-// 	}
-// 	slides[slideIndex-1].style.display = "block";  
-// 	dots[slideIndex-1].className += " active";
-// 	setTimeout(showSlides, 2000); // Change image every 2 seconds
-//   }
-
-
   $(function() {
   
 	// contact form animations
@@ -217,4 +189,50 @@ function openCity(evt, cityName) {
 	
   });
 
+
+
+
+
+
+
+
+// gallery
+
+
+  function openModal() {
+	document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+	document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+	showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+	showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("demo");
+	var captionText = document.getElementById("caption");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+	captionText.innerHTML = dots[slideIndex-1].alt;
+  }
 
